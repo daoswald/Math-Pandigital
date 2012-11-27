c20749480adc62132c80ed987029a2d7a97ea0f0
David Oswald <daoswald@gmail.com>
2012-11-27 02:52:43 -0800

    POD improvements.

--- lib/Math/Pandigital.pm
+++ lib/Math/Pandigital.pm
@@ -10,124 +10,125 @@ use Carp;
 our $VERSION = '0.01';
 
 has base   => ( is => 'ro', isa => 'Int',  default => sub { 10; } );
-has unique => ( is => 'ro', isa => 'Bool', default => sub { 0;  } );
-has zero   => ( is => 'ro', isa => 'Bool', default => sub { 1;  } );
+has unique => ( is => 'ro', isa => 'Bool', default => sub { 0; } );
+has zero   => ( is => 'ro', isa => 'Bool', default => sub { 1; } );
 
 has _digits_array => (
-  is      => 'ro',
-  isa     => 'ArrayRef',
-  lazy    => 1,
-  builder => '_build_digits_array'
+    is      => 'ro',
+    isa     => 'ArrayRef',
+    lazy    => 1,
+    builder => '_build_digits_array'
 );
 
 has _digits_regexp => (
-  is      => 'ro',
-  isa     => 'RegexpRef',
-  lazy    => 1,
-  builder => '_build_digits_regexp'
+    is      => 'ro',
+    isa     => 'RegexpRef',
+    lazy    => 1,
+    builder => '_build_digits_regexp'
 );
 
 has _min_length => (
-  is      => 'ro',
-  isa     => 'Int',
-  lazy    => 1,
-  builder => '_build_min_length'
+    is      => 'ro',
+    isa     => 'Int',
+    lazy    => 1,
+    builder => '_build_min_length'
 );
 
 sub BUILD {
-  my $self = shift;
-  croak "Base must be 1 .. 10, or 16"
-    unless $self->base > 0 && ( $self->base <= 10 || $self->base == 16 );
-  croak "Unary base can't exclude '0' (Don't un-set attrib zero for base 1)."
-    if $self->base == 1 && $self->zero == 0;
-  return;
+    my $self = shift;
+    croak "Base must be 1 .. 10, or 16"
+      unless $self->base > 0 && ( $self->base <= 10 || $self->base == 16 );
+    croak "Unary base can't exclude '0' (Don't un-set attrib zero for base 1)."
+      if $self->base == 1 && $self->zero == 0;
+    return;
 }
 
 sub _build_digits_array {
-  my $self  = shift;
-  my $start = $self->zero ? 0 : 1;
-  my $base  = $self->base;
-  my $set;
-  if( $base <= 10 ) {                     # Base 1 .. 10.
-    $set = [ $start .. $base - 1 ];
-  }
-  else {                                  # Base 16.
-    $set = [ $start .. 9, 'A' .. 'F' ];
-  }
-  return $set;
+    my $self  = shift;
+    my $start = $self->zero ? 0 : 1;
+    my $base  = $self->base;
+    my $set;
+    if ( $base <= 10 ) {    # Base 1 .. 10.
+        $set = [ $start .. $base - 1 ];
+    }
+    else {                  # Base 16.
+        $set = [ $start .. 9, 'A' .. 'F' ];
+    }
+    return $set;
 }
 
 sub _build_digits_regexp {
-  my $self = shift;
+    my $self = shift;
 
-  # Calculate the quantifier.
-  my $min_length = $self->_min_length;
-  my $quantifier = $self->unique ? "{$min_length}" : "{$min_length,}";
+    # Calculate the quantifier.
+    my $min_length = $self->_min_length;
+    my $quantifier = $self->unique ? "{$min_length}" : "{$min_length,}";
 
-  # Compose a regex string with character class and quantifier.
-  # Will look similar to "(?i:^[0123456789]{4,})", for example.
-  my $re_str = join( '', '(?i:^[', @{ $self->_digits_array() }, "]$quantifier)\$" );
-  # Turn it into a Regexp object and return.
-  return qr/$re_str/;
+    # Compose a regex string with character class and quantifier.
+    # Will look similar to "(?i:^[0123456789]{4,})", for example.
+    my $re_str =
+      join( '', '(?i:^[', @{ $self->_digits_array() }, "]$quantifier)\$" );
+
+    # Turn it into a Regexp object and return.
+    return qr/$re_str/;
 }
 
 sub _build_min_length {
-  my $self = shift;
+    my $self = shift;
 
-  # Calculate the minimum possible input length for $value to qualify.
-  return $self->base - ( $self->zero ? 0 : 1 );
+    # Calculate the minimum possible input length for $value to qualify.
+    return $self->base - ( $self->zero ? 0 : 1 );
 }
 
 sub is_pandigital {
-  my ( $self, $value ) = @_;
+    my ( $self, $value ) = @_;
 
-  return if not $self->_length_ok( $value );
+    return if not $self->_length_ok($value);
 
-  # The regexp test is done before we proceed to even more work.
-  return if not $self->_chars_ok( $value );
+    # The regexp test is done before we proceed to even more work.
+    return if not $self->_chars_ok($value);
 
-  # Finally count individual digits to verify we have enough of each digit.
-  return $self->_count_ok( $value );
+    # Finally count individual digits to verify we have enough of each digit.
+    return $self->_count_ok($value);
 
 }
 
-
-
 sub _length_ok {
-  my( $self, $value ) = @_;
+    my ( $self, $value ) = @_;
 
-  # Not pandigital if length is too short to contain all digits from base.
-  my $min_length = $self->_min_length;
-  return if length $value < $min_length;
+    # Not pandigital if length is too short to contain all digits from base.
+    my $min_length = $self->_min_length;
+    return if length $value < $min_length;
 
-  # Not pandigital if digits are unique, and length exceeds number
-  # of base digits.
-  return if $self->unique && length $value != $min_length;
+    # Not pandigital if digits are unique, and length exceeds number
+    # of base digits.
+    return if $self->unique && length $value != $min_length;
 
-  # Length seems fine.
-  return 1;
+    # Length seems fine.
+    return 1;
 }
 
 sub _chars_ok {
-  my( $self, $value ) = @_;
+    my ( $self, $value ) = @_;
 
-  # Reject if $value contains characters that are illegal for given base.
-  my $re = $self->_digits_regexp;
-  return $value =~ m/$re/; # Hex will be case-insensitive.
+    # Reject if $value contains characters that are illegal for given base.
+    my $re = $self->_digits_regexp;
+    return $value =~ m/$re/;    # Hex will be case-insensitive.
 }
 
 sub _count_ok {
-  my( $self, $value ) = @_;
-  
-  # Count occurrences of each digit in $value.
-  my %hash;
-  for my $digit ( split //, uc $value ) {
-    # If uniqueness is required, NOT pandigital if input has repeated digits.
-    return if ++$hash{$digit} > 1 && $self->unique;
-  }
+    my ( $self, $value ) = @_;
 
-  # Only pandigital if every digit in 'base' was touched at least once.
-  return keys %hash == $self->_min_length;
+    # Count occurrences of each digit in $value.
+    my %hash;
+    for my $digit ( split //, uc $value ) {
+
+       # If uniqueness is required, NOT pandigital if input has repeated digits.
+        return if ++$hash{$digit} > 1 && $self->unique;
+    }
+
+    # Only pandigital if every digit in 'base' was touched at least once.
+    return keys %hash == $self->_min_length;
 }
 
 __PACKAGE__->meta->make_immutable();
@@ -167,11 +168,12 @@ base system.  For example, a base-2 pandigital number must contain both 0 and 1.
 A base-10 pandigital number must contain 0, 1, 2, 3, 4, 5, 6, 7, 8, and 9.
 
 Pandigital numbers usually include zero.  However, zeroless pandigital numbers,
-containing (in base-10), 1 .. 9, and not 0 are sometimes permitted.
+containing (in base-10), 1 .. 9, and not 0 are sometimes permitted.  This module
+can accommodate that need.
 
 Additionally, some uses of pandigital numbers require that there be no repeated
 digits.  In such a case, the base-2 number 01 would be pandigital, whereas 101
-would not.
+would not.  Again, this module accommodates that possibility.
 
 L<Math::Pandigital> provides a class that can be instantiated in any base, from
 1 through 10, or 16 (hex), and can be used to detect pandigital numbers.  It may
@@ -201,12 +203,12 @@ test object.
     my $p = Math::Pandigital->new( base => 16 );
 
 Set's the base to any value from 1 to 10, or 16.  If the goal is to detect
-pandigitality of a binary number, select C<base => 2>, for example.  If not
+pandigitality of a binary number, select C<< base => 2 >>, for example.  If not
 specified, the default is base ten.  Common options are 2 (binary), 8 (octal),
-10 (decimal), and 16 (hex), though 1 is permitted (unary), as is any value
+10 (decimal), and 16 (hex). Base 1 is permitted (unary), as is any value
 between one and ten, inclusive.
 
-For base-16 tests, the digits C<A .. F> will be tested case-insensitively.
+For base-16 tests, the digits C<A..F> will be treated case-insensitively.
 
 =head4 unique
 
@@ -227,11 +229,13 @@ A Boolean flag.  The default is true.  When set (or the default accepted), the
 pandigital number must include a zero.  When unset, the pandigital number may
 not include a zero.
 
+A brief example:
+
     my $p = Math::Pandigital->new( base => 4, zero => 0, unique => 1 );
 
-The preceeding test would allow the following numbers to match:  123, 231, 213,
-321, 312, and 132.  It would reject any number with a zero, or more (or less)
-than three digits.
+The preceeding instantiation would set up a test that allows the following
+numbers to match:  123, 231, 213, 321, 312, and 132.  It would reject any number
+with a zero, or more (or less) than three digits.
 
 =head2 is_pandigital
 
@@ -241,11 +245,12 @@ C<$n> may be any string.  If the string contains only numeric digits that match
 the criteria set forth when the test object is constructed, true is returned.
 If the string contains any digits that aren't part of the base, or if it fails
 to contain all necessary digits, or if it violates the uniqueness setting (if
-set), it will return false.
+set), it will return false.  The letters 'A' through 'F' (case insensitive) are
+considered numeric digits when operating in base 16 mode.
 
-Beware the possible traps and pitfalls.  Perl sees C<0123456789> as
+Beware the possible traps and pitfalls.  Perl sees the integer, C<0123456789> as
 C<123456789>, which may not be what you were expecting.  If there's a
-possibility of a significant leadign zero, be sure to pass a string of digits
+possibility of a significant leading zero, be sure to pass a string of digits
 rather than a number.  C<is_pandigital> will silently stringify its target
 before testing, but if an integer with a leading '0' is passed as a parameter,
 the damage is done before C<is_pandigital> gets a chance to stringify it.
@@ -259,6 +264,16 @@ internally the string of digits is treated just as that, a I<string> of digits.
 This is true of any base system.  It just works out better that way.
 
 Remember that hex will be treated case-insensitively.
+
+Setting a base of 1 (unary), and a zero of false (zeros precluded) will cause
+the constructor to throw an exception; it makes no sense to exclude the only
+digit legal for unary.
+
+The C<is_pandigital> test tries to disqualify a string of digits in the least
+computationally expensive way possible first, and then moves to progressively
+more demanding tests.  First length is tested, then a regex is used to
+ensure only permissible digits occur, and finally a hash is used to count
+occurrences of each digit, ensuring a minimum of one of each.
 
 =head1 CAVEATS & WARNINGS
 
